c6496f45daaa2b230adedd0bb46bc7ba2739e6b8
Justas <36419298+juspaz@users.noreply.github.com>
2019-10-29 09:32:12 +0200

    Adding Factory pattern that makes ships.

--- WsApp/Controllers/ShipsController.cs
+++ WsApp/Controllers/ShipsController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WsApp.Models;
+using WsApp.Interfaces;
+using WsApp.Factory;
 
 namespace WsApp.Controllers
 {
-    public class ShipsController : Controller
+    public class ShipsController : Controller, IShip
     {
         private Context _context;
 
@@ -18,7 +20,9 @@ namespace WsApp.Controllers
         }
         public bool AddShip(int cellId,int shipTypeId, string shipType)
         {
-            Ship tempShip = new Ship();
+            //Ship tempShip = new Ship();
+            ShipFactory shipFactory = new ShipFactory();
+            Ship tempShip = shipFactory.GetShip("NormalShip");
             tempShip.CellId = cellId;
             tempShip.Name = shipType;
             tempShip.ShipTypeId = shipTypeId;
---
+++ WsApp/Factory/ShipFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WsApp.Models;
+
+namespace WsApp.Factory
+{
+    public class ShipFactory
+    {
+        public Ship GetShip(string shipType)
+        {
+            if (shipType.Equals("NormalShip"))
+            {
+                return new Ship();
+            } else if (shipType.Equals("ArmoredShip"))
+            {
+                //We might need a different ship, like ArmoreShip or something.
+                //Also possible to add a second constructor to ship and add armor that way.
+                return new Ship();
+            } else
+            {
+                //If there's a different ship type still return a shippy.
+                //Remove this later
+                return new Ship();
+            }
+        }
+    }
+}
